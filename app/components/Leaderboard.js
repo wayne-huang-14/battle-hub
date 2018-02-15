a2ce55ac74dedb10cbece62d1ceace080a4d01cf
Wayne Huang <wayne.huang14@gmail.com>
2018-02-15 00:13:56 -0800

    Moves Select Language Group to a stateless functional component

--- app/components/Leaderboard.js
+++ app/components/Leaderboard.js
@@ -1,5 +1,38 @@
 var React = require('react');
+var PropTypes = require('prop-types');
 import { Button } from 'semantic-ui-react'
+
+/**
+ * Select Language group that displays the language buttons.
+ *
+ * @param {Object} props
+ * @returns {*}
+ * @constructor
+ */
+function SelectLanguageGroup(props) {
+  var languages = ['All', 'JavaScript', 'Python', 'Ruby', 'Java', 'CSS'];
+  
+  return (
+    <div>
+    {languages.map(function(language) {
+      return (
+        <Button
+        style={props.selectedLanguage === language ? {color: 'blue'} : null}
+        key={language}
+        onClick={props.handleOnClick.bind(null, language)}
+        >
+          {language}
+        </Button>
+      )
+    })}
+    </div>
+  )
+}
+
+SelectLanguageGroup.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  handleOnClick: PropTypes.func.isRequired
+};
 
 class Leaderboard extends React.Component {
   constructor(props) {
@@ -21,21 +54,12 @@ class Leaderboard extends React.Component {
   }
   
   render() {
-    var languages = ['All', 'JavaScript', 'Python', 'Ruby', 'Java', 'CSS'];
-  
     return (
       <div>
-        {languages.map(function(language) {
-          return (
-            <Button
-              style={this.state.selectedLanguage === language ? {color: 'blue'} : null}
-              key={language}
-              onClick={this.updateLanguage.bind(this, language)}
-            >
-              {language}
-            </Button>
-          )
-        }.bind(this))}
+        <SelectLanguageGroup
+          selectedLanguage={this.state.selectedLanguage}
+          handleOnClick={this.updateLanguage.bind(this)}
+        />
       </div>
     )
   }
