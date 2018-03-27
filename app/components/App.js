37d861d175c8e51e206ba86ec047d6bba5f82a87
Wayne Huang <wayne.huang14@gmail.com>
2018-03-27 14:38:37 -0700

    Allows nav routes to scroll to top
    * Installs babel transform object rest spread plugin

--- app/components/App.js
+++ app/components/App.js
@@ -3,7 +3,8 @@ import Home from './Home';
 import Battle from './Battle';
 import Leaderboard from './Leaderboard';
 import Nav from './Nav';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ScrollToTopRoute from './ScrollToTopRoute';
 import Results from './Results';
 
 class App extends React.Component {
@@ -13,11 +14,11 @@ class App extends React.Component {
         <div className="container">
           <Nav/>
           <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/battle" component={Battle}/>
-            <Route path="/battle/results" component={Results}/>
-            <Route path="/leaderboard" component={Leaderboard}/>
-            <Route render={() => <p>Page Not Found</p>} />
+            <ScrollToTopRoute exact path="/" component={Home}/>
+            <ScrollToTopRoute exact path="/battle" component={Battle}/>
+            <ScrollToTopRoute path="/battle/results" component={Results}/>
+            <ScrollToTopRoute path="/leaderboard" component={Leaderboard}/>
+            <ScrollToTopRoute render={() => <p>Page Not Found</p>} />
           </Switch>
         </div>
       </Router>
